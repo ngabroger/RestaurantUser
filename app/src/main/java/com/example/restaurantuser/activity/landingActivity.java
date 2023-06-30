e05017e160cc4e7c13986a5b7c0da29d0f7b0379
Roger Simanjuntak <arbination13@gmail.com>
2023-06-30 18:41:41 +0700

    push project

--- app/src/main/java/com/example/restaurantuser/activity/landingActivity.java
+++ app/src/main/java/com/example/restaurantuser/activity/landingActivity.java
@@ -3,14 +3,24 @@ package com.example.restaurantuser.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.restaurantuser.R;
 
 public class landingActivity extends AppCompatActivity {
-
+private Button singIn;
+private TextView signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        initView();
     }
+
+    private void initView() {
+        singIn= findViewById(R.id.signInBtn);
+        signUp =findViewById(R.id.signUpBtn);
+    }
+
 }
